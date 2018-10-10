93e27843917b4c07f4be91d8cb7c0de8a72c4334
derrick b <dbozich1@gmail.com>
2018-10-10 01:44:10 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,52 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  let index = 0;
 
+  document.body.addEventListener('keydown', event => {
+    console.log(event.which);
+
+    let input = parseInt(event.which || event.detail);
+    if (code[index] === input) {
+      console.log(index);
+      index ++;
+      if (index  == code.length) {
+
+        alert("Hurray!");
+        index = 0;
+      }
+
+
+    } else {
+      index = 0;
+    }
+  });
 }
+
+init();
+
+
+
+// const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+//
+// function init() {
+//   let index = 0;
+//
+//   document.body.addEventListener('keydown', function(e) {
+//     const key = parseInt(e.which || e.detail);
+//
+//     if (code[index] === key) {
+//       index++;
+//
+//       if (index === code.length) {
+//         alert('Hurray!');
+//
+//         index = 0;
+//       }
+//     } else {
+//       index = 0;
+//     }
+//   });
+// }
+//
+// init();
