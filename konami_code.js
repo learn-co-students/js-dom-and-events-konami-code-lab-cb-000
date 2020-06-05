1281187d3459075d995fe00e40f97d1ff7dbcf7f
s-blais <stephen@s-blais.com>
2020-06-05 12:07:18 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,27 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  // Write your JavaScript code inside the init() function
+  
+  const body = document.querySelector('body');
+
+  let index = 0;
+
+  body.addEventListener('keydown', onKeyDownHandler);
+
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+      index++;
+      // console.log('keep going');
+      if (index === code.length) {
+        alert('Beep boop beep boop');
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+
+  };
 
 }
